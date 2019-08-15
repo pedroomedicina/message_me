9d66c19c233dff22a22bacb59a4689145c34e3fe
pedroomedicina <pedromedina190@gmail.com>
2019-08-15 19:20:44 -0400

    display messages and Add message partial and refactor code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "pedro", password:"123qwe")
+User.create(username: "blec", password:"123qwe")
+User.create(username: "pancho", password:"123qwe")
+User.create(username: "rai", password:"123qwe")
+
+Message.create(body: "Welcome!", user: User.find(1))
+Message.create(body: "bienvenido!", user: User.find(2))
+Message.create(body: "welkom!", user: User.find(3))
+Message.create(body: "herzlich willkommen!", user: User.find(4))
+Message.create(body: "Witamy!", user: User.find(1))
